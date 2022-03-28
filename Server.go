8f701317c202b0659fccda7bb658ca0c586a5607
Alexander Shevchenko <shevchenkoalexander@icloud.com>
2022-03-28 02:26:57 -0400

    cleanup

--- Server.go
+++ Server.go
@@ -1,85 +1,20 @@
 package main
 
 import (
-	"strconv"
-
 	"github.com/gin-gonic/gin"
 )
 
 func (g *GossipProtocol) ListenForRequests() {
-	node := g.list.LocalNode()
-
 	r := gin.Default()
+	r.Use(func(ctx *gin.Context) { ctx.Set("GOSSIP", g) })
 
-	r.GET("/gossip/:block", func(c *gin.Context) {
-		block, err := strconv.Atoi(c.Param("block"))
-		if err != nil {
-			c.JSON(400, gin.H{"message": "Invalid block number"})
-			return
-		}
-
-		if block < 0 || block > g.blockchain.lastBlock.NodeIndex {
-			c.JSON(400, gin.H{"message": "Block number out of range"})
-			return
-		}
-
-		current := g.blockchain.start
-		for i := 0; i < g.blockchain.lastBlock.NodeIndex; i++ {
-			if i == current.NodeIndex {
-				c.JSON(200, gin.H{"message": "Block found", "block": g.blockchain.lastBlock.Val[:]})
-				return
-			}
-			current = current.Next
-		}
-
-		c.JSON(404, gin.H{"message": "Block not found"})
-	})
-
-	r.GET("/gossip/:block/:batch", func(c *gin.Context) {
-		block, err := strconv.Atoi(c.Param("block"))
-		if err != nil {
-			c.JSON(400, gin.H{"message": "Invalid block number"})
-			return
-		}
-
-		if block < 0 || block > g.blockchain.lastBlock.NodeIndex {
-			c.JSON(400, gin.H{"message": "Block number out of range"})
-			return
-		}
-
-		batch, err := strconv.Atoi(c.Param("batch"))
-		if err != nil {
-			c.JSON(400, gin.H{"message": "Invalid batch number"})
-			return
-		}
-
-		if batch > 100 {
-			c.JSON(400, gin.H{"message": "Batch number too large"})
-			return
-		}
-
-		blocksBatch := []Node{}
-
-		current := g.blockchain.start
-		found := 0
-
-		for i := 0; i < g.blockchain.lastBlock.NodeIndex; i++ {
-			if i == current.NodeIndex || found > 0 {
-				blocksBatch = append(blocksBatch, *current)
-				found++
-			}
-			if found == batch {
-				break
-			}
-			current = current.Next
-		}
-
-		c.JSON(200, gin.H{"message": "Blocks found", "blocks": blocksBatch})
-	})
+	r.GET("/gossip/:block", getBlock)
+	r.GET("/gossip/:block/:batch", getBlockBatch)
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
 
-	r.Run(node.Address())
+	localNode := g.list.LocalNode()
+	r.Run(localNode.Address())
 }
---
+++ ServerController.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getBlock(c *gin.Context) {
+	g := c.Value("GOSSIP").(*GossipProtocol)
+
+	block, err := strconv.Atoi(c.Param("block"))
+	if err != nil {
+		c.JSON(400, gin.H{"message": "Invalid block number"})
+		return
+	}
+
+	if block < 0 || block > g.blockchain.lastBlock.NodeIndex {
+		c.JSON(400, gin.H{"message": "Block number out of range"})
+		return
+	}
+
+	current := g.blockchain.start
+	for i := 0; i < g.blockchain.lastBlock.NodeIndex; i++ {
+		if i == current.NodeIndex {
+			c.JSON(200, gin.H{"message": "Block found", "block": g.blockchain.lastBlock.Val[:]})
+			return
+		}
+		current = current.Next
+	}
+
+	c.JSON(404, gin.H{"message": "Block not found"})
+}
+
+func getBlockBatch(c *gin.Context) {
+	g := c.Value("GOSSIP").(*GossipProtocol)
+
+	block, err := strconv.Atoi(c.Param("block"))
+	if err != nil {
+		c.JSON(400, gin.H{"message": "Invalid block number"})
+		return
+	}
+
+	if block < 0 || block > g.blockchain.lastBlock.NodeIndex {
+		c.JSON(400, gin.H{"message": "Block number out of range"})
+		return
+	}
+
+	batch, err := strconv.Atoi(c.Param("batch"))
+	if err != nil {
+		c.JSON(400, gin.H{"message": "Invalid batch number"})
+		return
+	}
+
+	if batch > 100 {
+		c.JSON(400, gin.H{"message": "Batch number too large"})
+		return
+	}
+
+	blocksBatch := []Node{}
+
+	current := g.blockchain.start
+	counter := 0
+
+	for i := 0; i < g.blockchain.lastBlock.NodeIndex; i++ {
+		if i == current.NodeIndex || counter > 0 {
+			blocksBatch = append(blocksBatch, *current)
+			counter++
+		}
+		if counter == batch {
+			break
+		}
+		current = current.Next
+	}
+
+	c.JSON(200, gin.H{"message": "Blocks found", "blocks": blocksBatch})
+}
